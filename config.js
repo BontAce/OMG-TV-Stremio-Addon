0239d752adf94bccc87bc5172e36f3fe042589e9
Antonello <50622699+mccoy88f@users.noreply.github.com>
2025-02-09 22:38:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-// Configurazione base predefinita
 const baseConfig = {
     port: process.env.PORT || 10000,
-    M3U_URL: process.env.M3U_URL || 'https://raw.githubusercontent.com/mccoy88f/OMG-TV-Stremio-Addon/refs/heads/main/link.playlist',
-    EPG_URL: process.env.EPG_URL || 'https://raw.githubusercontent.com/mccoy88f/OMG-TV-Stremio-Addon/refs/heads/main/link.epg',
+    M3U_URL: 'https://raw.githubusercontent.com/mccoy88f/OMG-TV-Stremio-Addon/refs/heads/main/link.playlist',
+    EPG_URL: 'https://raw.githubusercontent.com/mccoy88f/OMG-TV-Stremio-Addon/refs/heads/main/link.epg',
     enableEPG: true,
     PROXY_URL: process.env.PROXY_URL || null,
     PROXY_PASSWORD: process.env.PROXY_PASSWORD || null,
@@ -58,72 +57,67 @@ const baseConfig = {
     }
 };
 
-function createConfig(customParams = {}) {
-    // Crea una nuova configurazione basata su quella predefinita
-    const config = JSON.parse(JSON.stringify(baseConfig));
+function loadCustomConfig() {
+    const configOverridePath = path.join(__dirname, 'addon-config.json');
 
-    // Applica i parametri personalizzati se presenti
-    if (customParams.m3uUrl) {
-        config.M3U_URL = customParams.m3uUrl;
-    }
-    if (customParams.epgUrl) {
-        config.EPG_URL = customParams.epgUrl;
-    }
-    if (customParams.enableEpg !== undefined) {
-        config.enableEPG = customParams.enableEpg === 'true';
-    }
-    if (customParams.cacheInterval) {
-        config.cacheSettings.updateInterval = parseInt(customParams.cacheInterval);
-    }
-
-    // Funzioni helper per la gestione degli URL
-    config.getBaseUrl = function() {
-        if (this.DOMAIN) {
-            const domain = this.DOMAIN.replace(/\/$/, ''); // Rimuove eventuale slash finale
-            const subpath = this.SUBPATH ? '/' + this.SUBPATH.replace(/^\/|\/$/g, '') : '';
-            return `${domain}${subpath}`;
-        }
-        return `http://localhost:${this.port}`;
-    };
-
-    config.getManifestUrl = function() {
-        return `${this.getBaseUrl()}/manifest.json`;
-    };
-
-    config.updateEPGUrl = function(url) {
-        if (!this.EPG_URL && url) {
-            this.EPG_URL = url;
-        }
-    };
-
-    // Carica configurazioni personalizzate dal file se presente
     try {
-        const configOverridePath = path.join(__dirname, 'addon-config.json');
-        if (fs.existsSync(configOverridePath)) {
+        const addonConfigExists = fs.existsSync(configOverridePath);
+
+        if (addonConfigExists) {
+            baseConfig.M3U_URL = process.env.M3U_URL || baseConfig.M3U_URL;
+            baseConfig.EPG_URL = process.env.EPG_URL || baseConfig.EPG_URL;
+        }
+
+        if (addonConfigExists) {
             const customConfig = JSON.parse(fs.readFileSync(configOverridePath, 'utf8'));
-            
-            // Applica le configurazioni personalizzate dal file
-            if (customConfig.addonId) config.manifest.id = customConfig.addonId;
-            if (customConfig.addonName) config.manifest.name = customConfig.addonName;
-            if (customConfig.addonDescription) config.manifest.description = customConfig.addonDescription;
-            if (customConfig.addonVersion) config.manifest.version = customConfig.addonVersion;
-            if (customConfig.addonLogo) config.manifest.logo = customConfig.addonLogo;
-            
-            // Aggiorna il catalogo se necessario
-            if (customConfig.addonId || customConfig.addonName) {
-                config.manifest.catalogs[0].id = 'omg_plus_tv';
-                config.manifest.catalogs[0].name = 'OMG+ TV';
-            }
+
+            const mergedConfig = {
+                ...baseConfig,
+                manifest: {
+                    ...baseConfig.manifest,
+                    id: customConfig.addonId || baseConfig.manifest.id,
+                    name: customConfig.addonName || baseConfig.manifest.name,
+                    description: customConfig.addonDescription || baseConfig.manifest.description,
+                    version: customConfig.addonVersion || baseConfig.manifest.version,
+                    logo: customConfig.addonLogo || baseConfig.manifest.logo,
+                    catalogs: [{
+                        ...baseConfig.manifest.catalogs[0],
+                        id: addonConfigExists ? 'omg_plus_tv' : baseConfig.manifest.catalogs[0].id,
+                        name: addonConfigExists ? 'OMG+ TV' : baseConfig.manifest.catalogs[0].name
+                    }]
+                }
+            };
+
+            return mergedConfig;
         }
     } catch (error) {
         console.error('Errore nel caricare la configurazione personalizzata:', error);
     }
 
-    return config;
+    return baseConfig;
 }
 
-// Esporta sia la funzione createConfig che una configurazione predefinita
-module.exports = {
-    createConfig,
-    config: createConfig()
+const config = loadCustomConfig();
+
+// Funzione per ottenere l'URL base dell'addon
+config.getBaseUrl = function() {
+    if (this.DOMAIN) {
+        const domain = this.DOMAIN.replace(/\/$/, ''); // Rimuove eventuale slash finale
+        const subpath = this.SUBPATH ? '/' + this.SUBPATH.replace(/^\/|\/$/g, '') : '';
+        return `${domain}${subpath}`;
+    }
+    return `http://localhost:${this.port}`;
 };
+
+// Funzione per ottenere l'URL completo del manifest
+config.getManifestUrl = function() {
+    return `${this.getBaseUrl()}/manifest.json`;
+};
+
+config.updateEPGUrl = function(url) {
+    if (!this.EPG_URL && url) {
+        this.EPG_URL = url;
+    }
+};
+
+module.exports = config;
